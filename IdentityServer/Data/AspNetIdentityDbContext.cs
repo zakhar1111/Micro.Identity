a16db1dc0afaa249266076af6a1023761435a542
Seva Zakharchenko <vseva80@yandex.ru>
2024-07-14 00:45:33 -0600

    ASP.NET Identity to the Server project

---
+++ IdentityServer/Data/AspNetIdentityDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.Data;
+
+public class AspNetIdentityDbContext 
+    : IdentityDbContext
+{
+    AspNetIdentityDbContext(DbContextOptions<AspNetIdentityDbContext> opt)
+        : base(opt) { }
+}
--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -1,15 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly.GetName().Name;
 var defaultConnString = builder.Configuration.GetConnectionString("Default");
 
-//builder.Services.AddDbContext<PersistedGrantDbContext>(options => options.UseSqlServer(defaultConnString));
+builder.Services.AddDbContext<AspNetIdentityDbContext>(options =>
+    options.UseSqlServer(defaultConnString,
+        b => b.MigrationsAssembly(assembly)));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AspNetIdentityDbContext>();
 
 builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<IdentityUser>()
     .AddConfigurationStore(options =>
     {
         options.ConfigureDbContext = b =>
