69d7d3c14ce4afc80c35710adbf5d651e2613b38
Seva Zakharchenko <vseva80@yandex.ru>
2024-07-15 07:49:42 -0600

    IdentityServer QuickStart code

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -13,12 +13,10 @@ if (seed)
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly.GetName().Name;
-var defaultConnString = builder.Configuration.GetConnectionString("DefaultConnection");
+var defaultConnString = builder.Configuration.GetConnectionString("Default");
 
 if (seed)
-{
     SeedData.EnsureSeedData(defaultConnString);
-}
 
 builder.Services.AddDbContext<AspNetIdentityDbContext>(options =>
     options.UseSqlServer(defaultConnString,
@@ -43,6 +41,13 @@ builder.Services.AddIdentityServer()
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+app.UseRouting();
 app.UseIdentityServer();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.Run();
