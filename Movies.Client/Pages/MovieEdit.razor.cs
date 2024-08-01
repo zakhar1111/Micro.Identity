3da4d3071743ca0e3cd3e7b0d70cdd8c6961164b
Seva Zakharchenko <vseva80@yandex.ru>
2024-08-01 15:03:09 -0600

    ++ MovieEdit

--- Movies.Client/Pages/MovieEdit.razor.cs
+++ Movies.Client/Pages/MovieEdit.razor.cs
@@ -4,30 +4,45 @@ using Movies.Client.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text.Json;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
 
 
 namespace Movies.Client.Pages;
 
-public partial class CreateNew : ComponentBase
+public partial class MovieEdit : ComponentBase
 {
+    [Parameter]
+    public int Id { get; set; }
+
     private Movie movie = new Movie();
     [Inject] private IHttpClientFactory httpClientFactory { get; set; }
     [Inject] private IConfiguration Config { get; set; }
 
-    protected  async Task HandleValidSubmit()
+    protected override async Task OnInitializedAsync()
     {
         var client = httpClientFactory.CreateClient();
- 
         var apiUrl = Config["apiUrl"];
+
+        var result = await client.GetAsync($"{apiUrl}/api/Movies/{Id}");
+        result.EnsureSuccessStatusCode();
+
+    }
+
+    private async Task HandleValidSubmit()
+    {
+        var client = httpClientFactory.CreateClient();
+        var apiUrl = Config["apiUrl"];
+
+        movie.Id = this.Id;
         movie.Rating = "0.0";
         movie.ImageUrl = "images/src";
-        var jsonMovie = new StringContent(JsonSerializer.Serialize(movie),Encoding.UTF8,Application.Json);
 
-        var result = await client.PostAsync($"{apiUrl}/api/Movies", jsonMovie);
+        var jsonMovie = new StringContent(JsonSerializer.Serialize(movie), Encoding.UTF8, "application/json");
+
+        var result = await client.PutAsync($"{apiUrl}/api/Movies/{this.Id}", jsonMovie);
         result.EnsureSuccessStatusCode();
 
     }
