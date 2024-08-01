c1cb9a5537655bd2fb8a8068183064eb0d3ae3d3
Seva Zakharchenko <vseva80@yandex.ru>
2024-08-01 15:27:21 -0600

    ++ MovieDelete

---
+++ Movies.Client/Pages/MovieDelete.razor.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Movies.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Movies.Client.Pages;
+
+public partial class MovieDelete : ComponentBase
+{
+    [Parameter]
+    public int Id { get; set; }
+
+    private Movie movie = new Movie();
+
+    [Inject] private IHttpClientFactory httpClientFactory { get; set; }
+    [Inject] private IConfiguration Config { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var client = httpClientFactory.CreateClient();
+        var apiUrl = Config["apiUrl"];
+
+        var result = await client.GetAsync($"{apiUrl}/api/Movies/{Id}");
+        result.EnsureSuccessStatusCode();
+
+        movie = await result.Content.ReadFromJsonAsync<Movie>();
+
+    }
+
+    private async Task DeleteMovie()
+    {
+        var client = httpClientFactory.CreateClient();
+        var apiUrl = Config["apiUrl"];
+
+        movie.Id = this.Id;
+        movie.Rating = "0.0";
+        movie.ImageUrl = "images/src";
+
+        var jsonMovie = new StringContent(JsonSerializer.Serialize(movie), Encoding.UTF8, "application/json");
+
+        var result = await client.DeleteAsync($"{apiUrl}/api/Movies/{this.Id}");
+        result.EnsureSuccessStatusCode();
+
+    }
+}
