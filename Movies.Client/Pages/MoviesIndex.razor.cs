4b0143cbb791dd9a11a97c9cab40e8f4e699168d
Seva Zakharchenko <vseva80@yandex.ru>
2024-08-01 01:11:36 -0600

    ++ GET movies ++ blazor server Client

---
+++ Movies.Client/Pages/MoviesIndex.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Movies.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Movies.Client.Pages;
+
+public partial class MoviesIndex : ComponentBase
+{
+    private IEnumerable<Movie> movies;
+    [Inject] private IHttpClientFactory httpClientFactory { get; set; }
+    [Inject] private IConfiguration Config { get; set; }
+
+
+    protected override async Task OnInitializedAsync()
+    {
+        var client = httpClientFactory.CreateClient();
+        //client.BaseAddress = new Uri(Config["apiUrl"]);
+        //movies = (IEnumerable<Movie>) await client.GetFromJsonAsync<Movie>($"/api/Movies");
+
+        var apiUrl = Config["apiUrl"];
+        var result = await client.GetAsync($"{apiUrl}/api/Movies");
+        result.EnsureSuccessStatusCode();
+
+        movies = await result.Content.ReadFromJsonAsync<IEnumerable<Movie>>();
+
+    }
+}
