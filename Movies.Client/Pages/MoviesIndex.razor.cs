61fbfc7f45b87551ca135155a9530cd970a91e82
Seva Zakharchenko <vseva80@yandex.ru>
2024-08-02 17:36:41 -0600

    ++ JWT to httpClient for Authorization

--- Movies.Client/Pages/MoviesIndex.razor.cs
+++ Movies.Client/Pages/MoviesIndex.razor.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
 using Movies.Client.Models;
+using Movies.Client.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 
@@ -14,13 +17,17 @@ public partial class MoviesIndex : ComponentBase
     private IEnumerable<Movie> movies;
     [Inject] private IHttpClientFactory httpClientFactory { get; set; }
     [Inject] private IConfiguration Config { get; set; }
+    [Inject] private ITokenService TokenService { get; set; }
 
 
     protected override async Task OnInitializedAsync()
     {
         var client = httpClientFactory.CreateClient();
-        //client.BaseAddress = new Uri(Config["apiUrl"]);
-        //movies = (IEnumerable<Movie>) await client.GetFromJsonAsync<Movie>($"/api/Movies");
+
+        //bearer token
+        var tokenResponse = await TokenService.GetToken("MoviesAPI.read");
+        client.SetBearerToken(tokenResponse.AccessToken);
+        //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenResponse.AccessToken);
 
         var apiUrl = Config["apiUrl"];
         var result = await client.GetAsync($"{apiUrl}/api/Movies");
