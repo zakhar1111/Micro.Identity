a152d08b605f91ab10de16f318d4623913148056
Seva Zakharchenko <vseva80@yandex.ru>
2024-08-05 13:58:38 -0600

    fix binding data = Form + model = update in DB

--- Movies.Client/Pages/MovieEdit.razor.cs
+++ Movies.Client/Pages/MovieEdit.razor.cs
@@ -8,6 +8,8 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Text;
 using System.Threading.Tasks;
+using Movies.Client.Services;
+using System.Net.Http.Headers;
 
 
 namespace Movies.Client.Pages;
@@ -21,27 +23,72 @@ public partial class MovieEdit : ComponentBase
     [Inject] private IHttpClientFactory httpClientFactory { get; set; }
     [Inject] private IConfiguration Config { get; set; }
 
+    [Inject] private ITokenService TokenService { get; set; }
+
     protected override async Task OnInitializedAsync()
     {
         var client = httpClientFactory.CreateClient();
+
+        //bearer token
+        var tokenResponse = await TokenService.GetToken("MoviesAPI.read");
+        //client.SetBearerToken(tokenResponse.AccessToken);
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenResponse.AccessToken);
+
         var apiUrl = Config["apiUrl"];
 
         var result = await client.GetAsync($"{apiUrl}/api/Movies/{Id}");
         result.EnsureSuccessStatusCode();
 
+        movie = await result.Content.ReadFromJsonAsync<Movie>();
+
     }
+
+    //private async Task HandleValidSubmit()
+    //{
+    //    var client = httpClientFactory.CreateClient();
+
+    //    //bearer token
+    //    var tokenResponse = await TokenService.GetToken("MoviesAPI.read");
+    //    //client.SetBearerToken(tokenResponse.AccessToken);
+    //    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenResponse.AccessToken);
+
+    //    var apiUrl = Config["apiUrl"];
+
+    //    movie.Id = this.Id;
+    //    movie.Rating = "0.0";
+    //    movie.ImageUrl = "images/src";
+
+    //    var jsonMovie = new StringContent(JsonSerializer.Serialize(movie), Encoding.UTF8, "application/json");
+
+    //    var result = await client.PutAsync($"{apiUrl}/api/Movies/{this.Id}", jsonMovie);
+    //    result.EnsureSuccessStatusCode();
+
+    //}
 
     private async Task HandleValidSubmit()
     {
         var client = httpClientFactory.CreateClient();
+
+        // Get bearer token
+        var tokenResponse = await TokenService.GetToken("MoviesAPI.read"); 
+        // Ensure you're using the correct scope
+        if (tokenResponse == null || string.IsNullOrEmpty(tokenResponse.AccessToken))
+        {
+            throw new Exception("Failed to retrieve token.");
+        }
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenResponse.AccessToken);
+
         var apiUrl = Config["apiUrl"];
 
+        // Prepare the movie data
         movie.Id = this.Id;
         movie.Rating = "0.0";
         movie.ImageUrl = "images/src";
 
+        // Serialize the movie object to JSON
         var jsonMovie = new StringContent(JsonSerializer.Serialize(movie), Encoding.UTF8, "application/json");
 
+        // Send the PUT request
         var result = await client.PutAsync($"{apiUrl}/api/Movies/{this.Id}", jsonMovie);
         result.EnsureSuccessStatusCode();
 
